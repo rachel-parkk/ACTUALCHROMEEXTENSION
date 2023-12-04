368d63f2bca434bfabe07ef262526464305c6b8c
rachel-parkk <102939391+rachel-parkk@users.noreply.github.com>
2023-12-04 11:52:05 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,125 +1,156 @@
-const timeInput = document.getElementById('time-input');
-const startButton = document.getElementById('start-button');
-const demoval=document.getElementById("demo");
-var list = '';
-const linkInput = document.getElementById("link-input");
-
 document.addEventListener("DOMContentLoaded", function () {
-    const textInput = document.getElementById("textInput");
-    const processButton = document.getElementById("processButton");
-    const viewAllButton = document.getElementById("viewAllButton");
-    const output = document.getElementById("output");
-    const processedTextList = document.getElementById("processedTextList");
-    let x = 0;
-  
-    // Initialize the processedTexts array with previously stored data (if any)
-    chrome.storage.sync.get({ processedTexts: [] }, function (data) {
-      processedTexts = data.processedTexts;
-    });
-  
-    //processButton.addEventListener("click", processText);
-    processButton.addEventListener("click", processText);
-    if (x==1){
-      viewAllButton.addEventListener("click", processedTextList.innerHTML="");
-      x=0;
-    }
-    else{
-      viewAllButton.addEventListener("click", showAllTexts);
-    }
-    //viewAllButton.addEventListener("click",processedTextList.innerHTML="");
-  
-    let processedTexts = [];
-  
-    function processText() {
-      const inputText = textInput.value;
-      const processedText = processInput(inputText);
+  const textInput = document.getElementById("textInput");
+  const processButton = document.getElementById("processButton");
+  const viewAllButton = document.getElementById("viewAllButton");
+  const output = document.getElementById("output");
+  const Delete = document.getElementById("Delete");
+  const processedTextList = document.getElementById("processedTextList");
+  let x = 0;
+  // Initialize the processedTexts array with previously stored data (if any)
+  chrome.storage.sync.get({ processedTexts: [] }, function (data) {
+    processedTexts = data.processedTexts;
+  });
+
+  let processedTexts = [];
+  processButton.addEventListener("click", processText);
+
+  function processText() {
+    const inputText = textInput.value;
+    const processedText = processInput(inputText);
+    if (processedTexts.includes(processedText)==false){
       // Store the processed text in the array
       processedTexts.push(processedText);
-  
       // Store the updated array in chrome.storage.sync
       chrome.storage.sync.set({ processedTexts });
-  
       // Display the processed text below the input field
-      output.textContent = processedText;
-  
-      // Clear the input field
-      textInput.value = "";
+      output.textContent = "Site Blocked: "+processedText;
     }
-  
-    function showAllTexts() {
-      x=1;
-      // Display all the processed texts in the container
-      processedTextList.style.display = "block";
-      processedTextList.innerHTML = processedTexts.map((text, index) => `<p>${index + 1}: ${text}</p>`).join("<br>");
+    else{
+      output.textContent = "Link Already Added!";
     }
-  
-    function processInput(inputText) {
-      inputText = inputText.replace(/.*?(www\.|https:\/\/)/, '');
-  
-      // Remove text after ".com", ".gov", or ".gov"
-      //inputText = inputText.replace(/(\.com|\.gov|\.gov).*/, '');
-      if (inputText.includes(".com")) {
-        inputText = inputText.substring(0, inputText.indexOf(".com")+4);
-      } else if (inputText.includes(".org")) {
-        inputText = inputText.substring(0, inputText.indexOf(".org")+4);
-      } else if (inputText.includes(".gov")) {
-        inputText = inputText.substring(0, inputText.indexOf(".gov")+4);
-      }
-      return inputText; 
-    }
-  });
-  
-
-startButton.addEventListener('click', function () {
-    const timeValue = timeInput.value;
-    if (validateTimeInput(timeValue)) {
-      // Disable the input field
-      timeInput.disabled = true;
-  
-      // Parse the time input into hours, minutes, and seconds
-      const [hours, minutes, seconds] = timeValue.split(':').map(Number);
-      let totalSeconds = hours * 3600 + minutes * 60 + seconds;
-      chrome.storage.set=({ countdownTime: totalSeconds });
-      // Function to update the countdown
-      function updateCountdown() {
-        chrome.storage.set=({ countdownTime: totalSeconds });
-        //chrome.storage.local.set({ countdownTime: totalSeconds });
-        const hoursRemaining = Math.floor(totalSeconds / 3600);
-        const minutesRemaining = Math.floor((totalSeconds % 3600) / 60);
-        const secondsRemaining = totalSeconds % 60;
-  
-        // Display the countdown in the input field
-        timeInput.value = `${String(hoursRemaining).padStart(2, '0')}:${String(minutesRemaining).padStart(2, '0')}:${String(secondsRemaining).padStart(2, '0')}`;
-  
-        // Check if the countdown has reached zero
-        if (totalSeconds <= 0) {
-          clearInterval(countdownInterval);
-          alert('Countdown has finished!');
-          timeInput.disabled=false;
-  
-        } else {
-          totalSeconds--;
-        }
-      }
-  
-      // Update the countdown every second
-      const countdownInterval = setInterval(updateCountdown, 1000);
-    } else {
-      alert('Invalid time format. Please use hh:mm:ss format.');
-    }
-  });
-  
-  function validateTimeInput(input) {
-    const timePattern = /^(\d{2}):(\d{2}):(\d{2})$/;
-    return timePattern.test(input);
+    // Clear the input field
+    textInput.value = "";
   }
 
-  chrome.storage.local.countdownIntervalget(["countdownTime"], function(result) {
-    const countdownTime = result.countdownTime;
-    if (countdownTime) {
-      const timeInput = document.getElementById("time-input");
-      timeInput.value = secondsToHms(countdownTime);
-      timeInput.disabled = true;
+  Delete.addEventListener('click', function() {
+    const inputValue = processInput(textInput.value);
+    const index = processedTexts.indexOf(inputValue);
+    if (index !==-1){
+      processedTexts.splice(index, 1);
+      chrome.storage.sync.set({ processedTexts });
+      output.textContent = 'Site has been unblocked!.';
+    }
+    else{
+      output.textContent = 'Site not found.';
+    }
+    textInput.value = "";
+
+  });
+
+  function processInput(inputText) {
+    inputText = inputText.replace(/.*?(www\.|https:\/\/)/, '');
+    inputText = inputText.replace(/.*?(www\.|https:\/\/)/, '');
+
+    if (inputText.includes(".com")) {
+      inputText = inputText.substring(0, inputText.indexOf(".com")+4);
+    } else if (inputText.includes(".org")) {
+      inputText = inputText.substring(0, inputText.indexOf(".org")+4);
+    } else if (inputText.includes(".gov")) {
+      inputText = inputText.substring(0, inputText.indexOf(".gov")+4);
+    } 
+    return inputText;
+  }
+  processButton.addEventListener("click", processText);
+  viewAllButton.addEventListener('click', function () {
+    if (x==0){
+      processedTextList.style.display = 'block';
+      processedTextList.innerHTML = processedTexts.map((text, index) => `<p>${text}</p>`).join("<br>");
+      x=1;
+    }
+    else{
+      processedTextList.style.display = 'none';
+      x=0;
+    }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const startButton = document.getElementById('startButton');
+    const timeInput = document.getElementById('timeInput');
+    const countdownElement = document.getElementById('countdown');
+  
+    startButton.addEventListener('click', function () {
+      const inputTime = parseTimeInput(timeInput.value);
+      if (inputTime>43200){
+        alert('Maximum time allowed is 12 hours. Please start practicing healthy working habits...');
+      }
+      else if (inputTime !== null) {
+        // Disable the time input field
+        timeInput.disabled = true;
+        // Send a message to the background script to start the countdown
+        chrome.runtime.sendMessage({ action: 'startCountdown', totalSeconds: inputTime }, function (response) {
+          if (response.started) {
+            console.log('Countdown started.');
+          }
+        });
+          // Get remaining seconds from the background script
+        chrome.runtime.sendMessage({ action: 'getRemainingSeconds' }, function (response) {
+          if (response.remainingSeconds > 0) {
+            timeInput.disabled = true;
+            // Update the time input field with the remaining time
+            updateCountdown(response.remainingSeconds);
+          }
+    });
+      } else {
+        alert('Invalid time format. Please enter time in HH:MM:SS.');
+      }
+    });
+  
+    // Get remaining seconds from the background script
+    chrome.runtime.sendMessage({ action: 'getRemainingSeconds' }, function (response) {
+      if (response.remainingSeconds > 0) {
+        // Update the time input field with the remaining time
+        updateCountdown(response.remainingSeconds);
+      }
+    });
+  
+    function parseTimeInput(input) {
+      const timeRegex = /^(\d{2}):(\d{2}):(\d{2})$/;
+      const match = input.match(timeRegex);
+      if (match) {
+        const hours = parseInt(match[1]);
+        const minutes = parseInt(match[2]);
+        const seconds = parseInt(match[3]);
+        return hours * 3600 + minutes * 60 + seconds;
+      }
+      return null;
+    }
+  
+    function updateCountdown(seconds) {
+      const interval = setInterval(function () {
+        if (seconds == 0) {
+          timeInput.disabled=false;
+        } 
+        if (seconds>0) {
+          timeInput.disabled = true;
+          //countdownElement.textContent = formatTime(seconds);
+          seconds--;
+          timeInput.value = formatTime(seconds);
+          timeInput.disabled = true;
+          // Update the time input field with the remaining time
+        }
+      }, 1000);
+    }
+  
+    function formatTime(seconds) {
+      const hours = Math.floor(seconds / 3600);
+      const minutes = Math.floor((seconds % 3600) / 60);
+      const secs = seconds % 60;
+      return `${pad(hours)}:${pad(minutes)}:${pad(secs)}`;
+    }
+  
+    function pad(number) {
+      return number < 10 ? `0${number}` : number;
     }
   });
   
