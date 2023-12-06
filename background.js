dd7287e3d14ee4130273f7d03a642a54c121812e
rachel-parkk <102939391+rachel-parkk@users.noreply.github.com>
2023-12-06 01:17:48 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 let countdownInterval;
 let remainingSeconds = 0;
+let y=0;
 
 function startCountdown(totalSeconds) {
   remainingSeconds = totalSeconds;
@@ -7,8 +8,10 @@ function startCountdown(totalSeconds) {
   countdownInterval = setInterval(function () {
     if (remainingSeconds <= 0) {
       clearInterval(countdownInterval);
+      y=0;
     } else {
       remainingSeconds--;
+      y=1;
 
       // Save countdown state to storage
       chrome.storage.sync.set({ countdown: remainingSeconds });
@@ -36,3 +39,21 @@ chrome.storage.sync.get(['countdown'], function (result) {
   }
 });
 
+chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
+  // Check if the URL contains any element from the list
+  chrome.storage.sync.get(["processedTexts"], function(data) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      const processedTexts = data.processedTexts || [];
+      for (const listItem of processedTexts) {
+        if (y>0){
+          const targetUrl = tabs[0].url.toLowerCase();
+          if (targetUrl.includes(listItem.toLowerCase())) {
+          // Redirect to "hello.com"
+            chrome.tabs.update(details.tabId, { url: "www.google.com" });
+          break; // Stop checking once a match is found
+          }
+    }
+    }
+  });
+});
+});
