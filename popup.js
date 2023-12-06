82a619eaa61c0029f26cce03064aad2ea7ab4333
rachel-parkk <102939391+rachel-parkk@users.noreply.github.com>
2023-12-06 01:18:27 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+let tf=0;
 document.addEventListener("DOMContentLoaded", function () {
   const textInput = document.getElementById("textInput");
   const processButton = document.getElementById("processButton");
@@ -6,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const Delete = document.getElementById("Delete");
   const processedTextList = document.getElementById("processedTextList");
   let x = 0;
+  //let tf=0;
   // Initialize the processedTexts array with previously stored data (if any)
   chrome.storage.sync.get({ processedTexts: [] }, function (data) {
     processedTexts = data.processedTexts;
@@ -16,35 +18,41 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function processText() {
     const inputText = textInput.value;
-    const processedText = processInput(inputText);
-    if (processedTexts.includes(processedText)==false){
-      // Store the processed text in the array
-      processedTexts.push(processedText);
-      // Store the updated array in chrome.storage.sync
-      chrome.storage.sync.set({ processedTexts });
-      // Display the processed text below the input field
-      output.textContent = "Site Blocked: "+processedText;
-    }
-    else{
-      output.textContent = "Link Already Added!";
-    }
-    // Clear the input field
-    textInput.value = "";
+    if (inputText.trim().length!=0){
+      const processedText = processInput(inputText);
+      if (processedTexts.includes(processedText)==false){
+        // Store the processed text in the array
+        processedTexts.push(processedText);
+        // Store the updated array in chrome.storage.sync
+        chrome.storage.sync.set({ processedTexts });
+        // Display the processed text below the input field
+        output.textContent = "Site Blocked: "+processedText;
+      }
+      else{
+        output.textContent = "Link Already Added!";
+      }
+      // Clear the input field
+      textInput.value = "";
   }
+  else{
+    output.textContent = "Enter Valid Input";
+  }
+}
 
   Delete.addEventListener('click', function() {
-    const inputValue = processInput(textInput.value);
-    const index = processedTexts.indexOf(inputValue);
-    if (index !==-1){
-      processedTexts.splice(index, 1);
-      chrome.storage.sync.set({ processedTexts });
-      output.textContent = 'Site has been unblocked!.';
-    }
-    else{
-      output.textContent = 'Site not found.';
-    }
-    textInput.value = "";
-
+    if (tf==0){
+      const inputValue = processInput(textInput.value);
+      const index = processedTexts.indexOf(inputValue);
+      if (index !==-1){
+        processedTexts.splice(index, 1);
+        chrome.storage.sync.set({ processedTexts });
+        output.textContent = 'Site has been unblocked!';
+      }
+      else{
+        output.textContent = 'Site not found.';
+      }
+      textInput.value = "";
+  }
   });
 
   function processInput(inputText) {
@@ -78,32 +86,36 @@ document.addEventListener('DOMContentLoaded', function () {
     const startButton = document.getElementById('startButton');
     const timeInput = document.getElementById('timeInput');
     const countdownElement = document.getElementById('countdown');
-  
+    //let tf=0;
+
     startButton.addEventListener('click', function () {
-      const inputTime = parseTimeInput(timeInput.value);
-      if (inputTime>43200){
-        alert('Maximum time allowed is 12 hours. Please start practicing healthy working habits...');
-      }
-      else if (inputTime !== null) {
-        // Disable the time input field
-        timeInput.disabled = true;
-        // Send a message to the background script to start the countdown
-        chrome.runtime.sendMessage({ action: 'startCountdown', totalSeconds: inputTime }, function (response) {
-          if (response.started) {
-            console.log('Countdown started.');
-          }
-        });
-          // Get remaining seconds from the background script
-        chrome.runtime.sendMessage({ action: 'getRemainingSeconds' }, function (response) {
-          if (response.remainingSeconds > 0) {
-            timeInput.disabled = true;
-            // Update the time input field with the remaining time
-            updateCountdown(response.remainingSeconds);
-          }
-    });
-      } else {
-        alert('Invalid time format. Please enter time in HH:MM:SS.');
-      }
+      if (tf==0){
+        const inputTime = parseTimeInput(timeInput.value);
+        if (inputTime>43200){
+          alert('Maximum time allowed is 12 hours. Please start practicing healthy working habits...');
+        }
+        else if (inputTime !== null) {
+          // Disable the time input field
+          timeInput.disabled = true;
+          // Send a message to the background script to start the countdown
+          chrome.runtime.sendMessage({ action: 'startCountdown', totalSeconds: inputTime }, function (response) {
+            if (response.started) {
+              console.log('Countdown started.');
+            }
+          });
+            // Get remaining seconds from the background script
+          chrome.runtime.sendMessage({ action: 'getRemainingSeconds' }, function (response) {
+            if (response.remainingSeconds > 0) {
+              tf=1;
+              timeInput.disabled = true;
+              // Update the time input field with the remaining time
+              updateCountdown(response.remainingSeconds);
+            }
+      });
+        } else {
+          alert('Invalid time format. Please enter time in HH:MM:SS.');
+        }
+    }
     });
   
     // Get remaining seconds from the background script
@@ -130,9 +142,11 @@ document.addEventListener('DOMContentLoaded', function () {
       const interval = setInterval(function () {
         if (seconds == 0) {
           timeInput.disabled=false;
+          tf=0;
         } 
         if (seconds>0) {
           timeInput.disabled = true;
+          tf=1;
           //countdownElement.textContent = formatTime(seconds);
           seconds--;
           timeInput.value = formatTime(seconds);
